08c7b448dae010695fc10041fcfece06102b1770
Altaf <safi.mohammad.altaf@gmail.com>
2024-08-12 20:38:50 -0500

    TDD

--- src/test/java/playground/Activity1.java
+++ src/test/java/playground/Activity1.java
@@ -3,17 +3,35 @@ package playground;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Activity1 {
 
+  private WebDriver webDriver;
+
+
+    @BeforeMethod
+public void SetupBrowser(){
+        webDriver = new ChromeDriver();
+        webDriver.get("https://www.facebook.com/");
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+}
 
     @Test
     public void FacebookTest(){
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.facebook.com/");
-        webDriver.manage().window().maximize();
-        String title = webDriver.getTitle();
-        System.out.println(title);
+        String Title = webDriver.getTitle();
+        Assert.assertEquals(Title,"Facebook - log in or sign up");
+    }
+
+
+    @AfterMethod
+    public void CleanUpTest(){
+        webDriver.quit();
     }
 }
--- src/test/java/playground/MyFirstTest.java
+++ src/test/java/playground/MyFirstTest.java
@@ -1,11 +1,16 @@
 package playground;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
 
+    @BeforeClass
+    public void runBeforeTestClass(){
+        System.out.println("This is only 1 Time per Test Class");
+    }
     @BeforeMethod
     public void runBeforeTest(){
         System.out.println("This is Before Each Test");
